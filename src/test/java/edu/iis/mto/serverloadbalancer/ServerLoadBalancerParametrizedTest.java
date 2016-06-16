c5242d92ba2608188e631a0b4e61c1753454e00b
Wojciech Pełka <nero1993@onet.pl>
2016-06-16 22:36:38 +0200

    ServerLoadBalancerParametrizedTest class has been extended to run test with parameters

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -5,16 +5,50 @@ import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
+	
+	private double capacity;
+	private double size;
+	private double load;
+	
+	
+	
+	
+	public ServerLoadBalancerParametrizedTest(double capacity, double size, double load) 
+	{
+		this.capacity = capacity;
+		this.size = size;
+		this.load = load;
+	}
+
+
+	//capacity,size,load
+	@Parameters
+	public static Collection<Double[]> balancingParams() 
+	{
+		return Arrays.asList(new Double[][] { { 1.0d, 1.0d, 100.0d }, { 2.0d, 1.0d, 50.0d },
+			{ 4.0d, 1.0d, 25.0d }, });
+	}
+	
+	
 	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() 
+	{
+		Server theServer = a(server().withCapacity((int)capacity));
+		Vm theVm = a(vm().ofSize((int)size));
 		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
+		assertThat(theServer, hasLoadPercentageOf(load));
 		assertThat("the server should contain vm", theServer.contains(theVm));
 	}
 	
